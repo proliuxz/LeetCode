4dba4a70403cff4aa853d84c7cf9a946906e1d89
Xinzhuo Liu <xinzhuo.liu@afusion.com>
2017-08-10 17:41:21 +0800

    20170810

--- src/solution651to700/Solution654.java
+++ src/solution651to700/Solution654.java
@@ -7,6 +7,25 @@ import Util.TreeNode;
  */
 public class Solution654 {
     public TreeNode constructMaximumBinaryTree(int[] nums) {
-        return null;
+        return constructHelper(nums, 0, nums.length);
+    }
+
+    public TreeNode constructHelper(int[] nums,int left, int right) {
+        if (left == right)
+            return null;
+        int maxIdx = findMax(nums, left, right);
+        TreeNode root = new TreeNode(nums[maxIdx]);
+        root.left = constructHelper(nums,left,maxIdx);
+        root.right = constructHelper(nums,maxIdx+1,right);
+        return root;
+    }
+
+    public int findMax(int[] nums, int left, int right) {
+        int max_i = left;
+        for (int i = left; i < right; i++) {
+            if (nums[max_i] < nums[i])
+                max_i = i;
+        }
+        return max_i;
     }
 }
